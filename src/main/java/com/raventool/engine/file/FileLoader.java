15b810a52979a9c27ba08fa29f33e29e37c2720b
Geo290 <jorgeemmanuel.2003@gmail.com>
2026-01-24 20:38:59 -0600

    feature: openFile() method renamed as loadFile() and removed resolvePath()

--- src/main/java/com/raventool/engine/file/FileLoader.java
+++ src/main/java/com/raventool/engine/file/FileLoader.java
@@ -1,45 +1,35 @@
 package com.raventool.engine.file;
 
-import java.io.IOException;
+import java.io.File;
 
-import com.raventool.model.request.RequestDetails;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.core.JacksonException;
 import tools.jackson.databind.JsonNode;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class FileLoader {
 
-    private ObjectMapper mapper;
-    private JsonNode root;
-    private String basePath;
+    private static ObjectMapper mapper = new ObjectMapper();
+    private static JsonNode rootNode = null;
 
-    public FileLoader(ObjectMapper mapper, JsonNode root) {
-        this.mapper = mapper;
-        this.root = root;
-        this.basePath = resolveBasePath();
+    public static void main(String[] args) {
+
+        try {
+            rootNode = loadFile("C://Users/Geo29/Documents/raventool/raven/tests/test.json");
+            System.out.println(rootNode.toPrettyString());
+
+        } catch (JacksonException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 
-    // private String resolveBasePath() {
-    //     String prop = System.getProperty("raventool.basepath");
-    //     if (prop != null && !prop.isBlank()) {
-    //         return prop;
-    //     }
-    //     String env = System.getenv("RAVENTOOL_BASEPATH");
-    //     if (env != null && !env.isBlank()) {
-    //         return env;
-    //     }
-    //     return "raven/tests";
-    // }
+    public static JsonNode loadFile(String path) throws IllegalArgumentException, JacksonException {
+        File file = new File(path);
 
-    protected void openFile(String filepath) throws IOException {
-        root = mapper.readTree(
-            Files.readString(Paths.get(basePath, filepath))
-        );
+        if (!file.exists()) {
+            throw new IllegalArgumentException("File does not exist: " + path);
+        }
+
+        return mapper.readTree(file);
     }
 
-    public RequestDetails loadFromFile(String filepath) {
-
-    }
 }
