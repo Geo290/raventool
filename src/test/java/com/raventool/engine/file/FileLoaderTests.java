6adbcb42c2b3d8b89e256a67a5233959c6208340
Geo290 <jorgeemmanuel.2003@gmail.com>
2026-02-02 11:59:44 -0600

    test: FileLoader class tests

---
+++ src/test/java/com/raventool/engine/file/FileLoaderTests.java
@@ -0,0 +1,32 @@
+package com.raventool.engine.file;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import tools.jackson.databind.JsonNode;
+
+public class FileLoaderTests {
+
+    public static String filePath = "C://Users/Geo29/Documents/raventool/raven/tests/test.json";
+    public static JsonNode root = FileLoader.loadFile(filePath);
+
+    @Test
+    void testIsFileLoaded() {
+        try {
+            assertTrue(root.has("url"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testIsJsonParsedIntoRequestDetails() {
+        try {
+            var requestDetails = FileLoader.parseRequestDetails(root);
+            assertTrue(requestDetails.getClass() == com.raventool.model.request.RequestDetails.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
