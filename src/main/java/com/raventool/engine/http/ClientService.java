c8839001e1efd3950d32c363064e13ff46c9a37d
Geo290 <jorgeemmanuel.2003@gmail.com>
2026-02-06 20:23:19 -0600

    feature: getTime() method created to meassure server response time

--- src/main/java/com/raventool/engine/http/ClientService.java
+++ src/main/java/com/raventool/engine/http/ClientService.java
@@ -3,6 +3,11 @@ package com.raventool.engine.http;
 import java.net.http.HttpResponse.*;
 import java.util.concurrent.CompletableFuture;
 
+import com.raventool.engine.file.FileLoader;
+import com.raventool.model.request.RequestDetails;
+
+import tools.jackson.databind.JsonNode;
+
 import java.net.http.HttpResponse;
 import java.net.http.HttpRequest;
 import java.net.http.HttpClient;
@@ -28,5 +33,20 @@ public class ClientService {
 
     public CompletableFuture<Object> getStatus(CompletableFuture<HttpResponse<String>> future) {
         return future.thenApplyAsync(HttpResponse::statusCode);
+    }
+
+    public static void getTime(CompletableFuture<HttpResponse<String>> future, long start) {
+        future.whenComplete((response, throwable) -> {
+            long end = System.nanoTime();
+            long durationMs = (end - start) / 1_000_000;
+
+            if (throwable != null) {
+                System.err.println("Request Failed after " + durationMs + " ms");
+                throwable.printStackTrace();
+
+            } else {
+                System.err.println("Response Time: " + durationMs + " ms");
+            }
+        });
     }
 }
