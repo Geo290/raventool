ced1b7a39d55ede7298bbe2a5897c22b7cde139a
Geo290 <jorgeemmanuel.2003@gmail.com>
2026-02-08 11:41:24 -0600

    refactor: getBody(), getStatus(), getTime() moved

--- src/main/java/com/raventool/engine/http/ClientService.java
+++ src/main/java/com/raventool/engine/http/ClientService.java
@@ -21,27 +21,4 @@ public class ClientService {
     public CompletableFuture<HttpResponse<String>> sendRequest() {
         return client.sendAsync(request, responseBodyHandlers);
     }
-
-    public CompletableFuture<String> getBody(CompletableFuture<HttpResponse<String>> future) {
-        return future.thenApplyAsync(HttpResponse::body);
-    }
-
-    public CompletableFuture<Object> getStatus(CompletableFuture<HttpResponse<String>> future) {
-        return future.thenApplyAsync(HttpResponse::statusCode);
-    }
-
-    public static void getTime(CompletableFuture<HttpResponse<String>> future, long start) {
-        future.whenComplete((response, throwable) -> {
-            long end = System.nanoTime();
-            long durationMs = (end - start) / 1_000_000;
-
-            if (throwable != null) {
-                System.err.println("Request Failed after " + durationMs + " ms");
-                throwable.printStackTrace();
-
-            } else {
-                System.err.println("Response Time: " + durationMs + " ms");
-            }
-        });
-    }
 }
