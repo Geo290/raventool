92a184f1dd25a655b793eb33e63e9161007c0c5f
Geo290 <jorgeemmanuel.2003@gmail.com>
2026-03-10 19:40:46 -0600

    Refactor: Displaying of test results

--- src/main/java/com/raventool/utils/TestResolver.java
+++ src/main/java/com/raventool/utils/TestResolver.java
@@ -1,58 +1,86 @@
 package com.raventool.utils;
+
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Map.Entry;
+
+import com.raventool.model.response.ResponseDetails;
 
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
 public class TestResolver {
 
-    // public static void main(String[] args) {
-    //     String json1 = """
-    //             {
-    //               "name": "Alice",
-    //               "age": 30,
-    //               "skills": ["Java", "Python"],
-    //               "address": { "city": "NY", "zip": "10001" }
-    //             }
-    //             """;
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode expectedNode = mapper.createObjectNode().put("language", "Java");
+        JsonNode actualNode = mapper.createObjectNode().put("language", "python");
 
-    //     String json2 = """
-    //             {
-    //               "name": "Alice",
-    //               "age": 30.0,
-    //               "skills": ["Java", "Go"],
-    //               "address": { "city": "NY", "zip": "10002" }
-    //             }
-    //             """;
+        ResponseDetails expectedResponse = new ResponseDetails(204, expectedNode, 98);
+        ResponseDetails actualResponse = new ResponseDetails(204, actualNode, 100);
 
-    //     ObjectMapper mapper = new ObjectMapper();
+        test(expectedResponse, actualResponse);
+    }
 
-    //     JsonNode node1 = mapper.readTree(json1);
-    //     JsonNode node2 = mapper.readTree(json2);
-    //     testResults(node1, node2);
+    public static boolean assertStatus(int expectedStatus, int actualStatus) {
+        return expectedStatus == actualStatus;
+    }
 
-    // }
+    public static boolean assertBody(JsonNode expectedResponseBody, JsonNode actualResponseBody) {
+        return Objects.equals(expectedResponseBody, actualResponseBody);
+    }
 
-    public static void testResults(JsonNode actualResponseBody, JsonNode expectedRespondeBody) {
-        Iterator<Entry<String, JsonNode>> expectedProps = expectedRespondeBody.properties().iterator();
+    public static boolean assertValue(Object expected, Object actual) {
+        return Objects.equals(expected, actual);
+    }
+
+    public static void test(ResponseDetails expectedResponse, ResponseDetails actualResponse) {
+        int expectedStatus = expectedResponse.status();
+        int actualStatus = actualResponse.status();
+        JsonNode expectedBody = expectedResponse.body();
+        JsonNode actualBody = actualResponse.body();
+
+        System.out.println("= = = = STARTING TESTS = = = =");
+
+        if (!assertStatus(actualStatus, expectedStatus)) {
+            System.out.println("STATUS ASSERTION FAILED");
+
+            System.out.printf(
+                    "Asserting Status[%d] == [%d] %s\n",
+                    expectedStatus,
+                    actualStatus,
+                    "FAILED");
+
+            System.out.println("ABORTING TESTS");
+            return;
+        } else {
+            System.out.printf(
+                    "Asserting Status[%d] == [%d] %s\n",
+                    expectedStatus,
+                    actualStatus,
+                    "SUCCEEDED");
+        }
+
+        Iterator<Entry<String, JsonNode>> expectedProps = expectedBody.properties().iterator();
 
         while (expectedProps.hasNext()) {
             Entry<String, JsonNode> entry = expectedProps.next();
             String key = entry.getKey();
             JsonNode valueE = entry.getValue();
-            JsonNode valueA = actualResponseBody.get(key);
+            JsonNode valueA = actualBody.get(key);
 
             if (valueE != null) {
-                // return false;
-                String mote = valueE.equals(valueA) ? " SUCCESS" : " FAIL";
-                System.out.println(valueE + " == " + valueA + mote);
+                String valueAsserted = assertValue(valueE, valueA) ? "PASSED" : "FAILED";
+                System.out.printf("Assert %s [%s] == [%s] %s\n",
+                        key.toUpperCase(),
+                        String.valueOf(valueE),
+                        String.valueOf(valueA),
+                        valueAsserted);
             }
 
             System.out.println("==================");
 
         }
         System.out.println("= = = = FINISHED = = = =");
-        // return actualResponseBody.size() == expectedRespondeBody.size();
     }
 }
